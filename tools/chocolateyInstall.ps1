6fbca8089a97e1a841099725a754e324cfd5c3a0
Adam Weiss <aweiss@synacor.com>
2015-01-18 14:29:51 -0500

    Fix chocolatey

--- tools/chocolateyInstall.ps1
+++
@@ -1,5 +0,0 @@
-$packageName = 'vigilantcupcake'
-$url = 'https://cdn.rawgit.com/amweiss/vigilant-cupcake/v0.2.0/VigilantCupcake/bin/Release/VigilantCupcake.exe'
-$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-
-Get-ChocolateyWebFile $packageName $unzipLocation $url
