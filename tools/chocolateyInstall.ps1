77d4bdbdf41534876e729fa4078bbeb2cf3a0573
Adam Weiss <aweiss@synacor.com>
2015-01-18 14:01:48 -0500

    Update chocolatey package

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'vigilantcupcake'
-$url = 'https://cdn.rawgit.com/amweiss/vigilant-cupcake/v0.0.1/VigilantCupcake/bin/Release/VigilantCupcake.exe'
+$url = 'https://cdn.rawgit.com/amweiss/vigilant-cupcake/v0.2.0/VigilantCupcake/bin/Release/VigilantCupcake.exe'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 Get-ChocolateyWebFile $packageName $unzipLocation $url
