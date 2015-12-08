5373d3f6d6f9b644405b3ba06dd665fdf26d0cc3
Adam Weiss <cseadam@gmail.com>
2015-12-08 12:53:30 -0500

    More appveyor tweaks

--- squirrel.ps1
+++ squirrel.ps1
@@ -1,43 +1,79 @@
 Properties {
-    $base_dir = resolve-path .
-    $src_dir = "$base_dir\VigilantCupcake"
-    $package_dir = "$base_dir\packages"
-    $release_dir = "$base_dir\Releases"
-    $squirrel = "$package_dir\squirrel.windows.*\tools\Squirrel.exe"
-    $syncReleases = "$package_dir\squirrel.windows.*\tools\SyncReleases.exe"
+	$base_dir = resolve-path .
+	$src_dir = "$base_dir\VigilantCupcake"
+	$package_dir = "$base_dir\packages"
+	$build_dir = "$src_dir\build"
+	$nuspec_dir = "$src_dir"
+	$temp_dir = "$build_dir\Temp"
+	$release_dir = "$base_dir\Releases"
+	$nuget = "$src_dir\.nuget\nuget.exe"
+	$squirrel = "$package_dir\squirrel.windows.*\tools\Squirrel.exe"
+	$syncReleases = "$package_dir\squirrel.windows.*\tools\SyncReleases.exe"
+}
+
+function Create-Package($project, $version) {
+	Create-Directory $temp_dir
+	Copy-Files "$nuspec_dir\$project.nuspec" $temp_dir
+
+	Try {
+		Replace-Content "$nuspec_dir\$project.nuspec" '0.0.0' $version
+		Exec { .$nuget pack "$nuspec_dir\$project.nuspec" -OutputDirectory "$build_dir" -BasePath "$base_dir" -Version $version -Properties Platform=AnyCPU -Properties Configuration=Release }
+	}
+	Finally {
+		Move-Files "$temp_dir\$project.nuspec" $nuspec_dir
+	}
 }
 
 function Get-BuildVersion {
-    $version = Get-SharedVersion
-    $buildNumber = $env:APPVEYOR_BUILD_NUMBER
+	$version = Get-SharedVersion
+	$buildNumber = $env:APPVEYOR_BUILD_NUMBER
 
-    if ($env:APPVEYOR_REPO_TAG -ne "True" -And $buildNumber -ne $null) {
-        $version += "-build-" + $buildNumber.ToString().PadLeft(5, '0')
-    }
+	if ($env:APPVEYOR_REPO_TAG -ne "True" -And $buildNumber -ne $null) {
+		$version += "-build-" + $buildNumber.ToString().PadLeft(5, '0')
+	}
 
-    return $version
+	return $version
 }
 
 function Get-SharedVersion {
-    $line = Get-Content "$sharedAssemblyInfo" | where {$_.Contains("AssemblyVersion")}
-    $line.Split('"')[1]
+	$line = Get-Content "$sharedAssemblyInfo" | where {$_.Contains("AssemblyVersion")}
+	$line.Split('"')[1]
+}
+
+function Create-Directory($dir) {
+	New-Item -Path $dir -Type Directory -Force > $null
+}
+
+function Copy-Files($source, $destination) {
+	Copy-Item "$source" $destination -Force > $null
+}
+
+function Move-Files($source, $destination) {
+	Move-Item "$source" $destination -Force > $null
+}
+
+
+function Replace-Content($file, $pattern, $substring) {
+	(gc $file) -Replace $pattern, $substring | sc $file
 }
 
 if ($env:APPVEYOR) {
-    $base_dir = $env:APPVEYOR_BUILD_FOLDER
-    $token = $env:GitHubToken
+	$base_dir = $env:APPVEYOR_BUILD_FOLDER
+	$token = $env:GitHubToken
 }
 
 Task Installer -Depends Pack -Description "Create Squirrel release." {
-    $version = Get-BuildVersion
-    
-    if ($token) {
-        Exec { .$syncReleases -releaseDir $release_dir -url "https://github.com/amweiss/vigilant-cupcake" -token $token }
-    } else {
-        Exec { .$syncReleases -releaseDir $release_dir -url "https://github.com/amweiss/vigilant-cupcake" }
-    }
-    Exec { .$squirrel -releasify "$build_dir\vigilantcupcake.$version.nupkg" -releaseDir $release_dir -setupIcon "$src_dir\VC2-nobg-whitecake.ico" } # -n "/a /f build/windows/app_signing.p12 /p SECRETPASSWORD"
+	$version = Get-BuildVersion
 
-    # Remove synced releases for github
-    Get-ChildItem "$release_dir.*" -exclude @('*' + $version + '*') | Remove-Item
+	Create-Package "vigilantcupcake" $version
+	
+	if ($token) {
+		Exec { .$syncReleases -releaseDir $release_dir -url "https://github.com/amweiss/vigilant-cupcake" -token $token }
+	} else {
+		Exec { .$syncReleases -releaseDir $release_dir -url "https://github.com/amweiss/vigilant-cupcake" }
+	}
+	Exec { .$squirrel -releasify "$build_dir\vigilantcupcake.$version.nupkg" -releaseDir $release_dir -setupIcon "$src_dir\VC2-nobg-whitecake.ico" } # -n "/a /f build/windows/app_signing.p12 /p SECRETPASSWORD"
+
+	# Remove synced releases for github
+	Get-ChildItem "$release_dir.*" -exclude @('*' + $version + '*') | Remove-Item
 }
